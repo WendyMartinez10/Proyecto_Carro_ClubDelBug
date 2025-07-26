9157c377b29af0f06e912bcecfde05c3562a4867
GipsyAgui <gipsyaguirre25@gmail.com>
2025-07-26 17:00:43 -0600

    Se crea la clase Radio

---
+++ src/Radio/Radio.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Radio;
+
+/**
+ *
+ * @author gipsy
+ */
+public class Radio {
+    private boolean Encedida;
+    private Modo modoActual;
+    
+}
