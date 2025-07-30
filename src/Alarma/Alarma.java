49f91dccea79efe774aadfd7e9d1577c973e5af8
GipsyAgui <gipsyaguirre25@gmail.com>
2025-07-30 17:49:34 -0600

    Se edita la clase alarma
    Se editan la clase alarma y sus metodos mejorando la clase

--- src/Alarma/Alarma.java
+++ src/Alarma/Alarma.java
@@ -13,10 +13,10 @@ public class Alarma {
     
     private boolean Alarma;
     private boolean bloqueoPuertas;
+    private Seguridad ModoActual;
 
     public Alarma() {
-        this.Alarma = true;
-        this.bloqueoPuertas = true;
+       setSeguridad(Seguridad.NORMAL);
     }
 
     public void activarAlarma() {
@@ -42,4 +42,27 @@ public class Alarma {
     public boolean isPuertasBloqueadas() {
         return bloqueoPuertas;
     }
+
+    public void setSeguridad(Seguridad ModoActual) {
+        this.ModoActual = ModoActual;
+        
+        switch (ModoActual) {
+            case NORMAL:
+                desactivarAlarma();
+                desbloquearPuertas();
+                break;
+            case NOCHE:
+            case VIAJE:
+                activarAlarma();
+                bloquearPuertas();
+                break;
+        }
+    }
+    public String Estado() {
+        return "Modo: " + ModoActual.name() +
+               " | " + ModoActual.getEstado() +
+               " | Alarma: " + (Alarma ? "Activada" : "Desactivada") +
+               " | Puertas: " + (bloqueoPuertas ? "Bloqueadas" : "Desbloqueadas");
+    }
 }
+        
