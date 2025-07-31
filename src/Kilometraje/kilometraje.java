99db3f1fa39e61d5515fac2b9d86bb751fc17997
Reynold <rruizo@est.utn.ac.cr>
2025-07-31 11:40:01 -0600

    modificacion de la clase kilometaje
    Se configura las conversiones de metros, km y demas para un mejor analisis de los metros y km recorridos junto con la velocidad

--- src/Kilometraje/kilometraje.java
+++ src/Kilometraje/kilometraje.java
@@ -12,11 +12,13 @@ public class kilometraje {
     private double totalKm;
     private int velocidad;
     private int rpm;
+    private int metros;
   
     public kilometraje(){
     totalKm = 0.0;
     velocidad = 0;
     rpm = 0;
+    metros = 0;
     }
   
     public void actualizarVelocidad(int nuevaVelocidad){
@@ -25,8 +27,13 @@ public class kilometraje {
     }
 
     public void recorer(int segundos){
-    double horas = segundos / 3600.0;
-    totalKm += velocidad * horas;
+        if (segundos<=0 || velocidad<=0) return;
+        double horas = segundos/3600.0;
+        double kmRecorridos = velocidad*horas;
+        double metrosRecorridos = kmRecorridos*1000;
+        
+        metros+=(int) metrosRecorridos;
+        totalKm+= metrosRecorridos/1000.0;
     }
 
     private int calcularRPM(int velocidad){
@@ -46,9 +53,9 @@ public class kilometraje {
     public int getRpm() {
         return rpm;
     }
+    
+    public int getMetros() {
+        return metros;
+    }
 
 }
-
-
-
-
